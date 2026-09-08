9c7e71e853bd4a4e9533289b54f6ff9d73276d5e
angelmanuelgl <go0zlo016@gmail.com>
2026-09-07 19:18:22 -0600

    1.2 copleted and AC

--- topics/GameTheory/1.2.KaosarAndGame.cpp
+++ topics/GameTheory/1.2.KaosarAndGame.cpp
@@ -10,7 +10,7 @@
                     "fake junmps" than the other, they can swap positions
     * Complexity: O(1) for each query
 
-    * Status: IN PROCESS
+    * Status: ACCEPTs
     * angelmanuelgl
 */
 #include<bits/stdc++.h>
